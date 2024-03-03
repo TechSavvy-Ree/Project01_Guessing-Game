4949a558802c27815bddb1d0ae8d1c94ddbe1b84
TechSavvy-Ree <ranya7476@gmail.com>
2024-03-02 13:57:49 -1200

    Project01_Number guessing game

---
+++ game.ts
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber: number = Math.floor(Math.random()*100)+1
+let remainingChances=6;
+console.log(randomNumber);
+function validNumber(input:string): boolean| string{
+
+    const number= parseFloat(input);
+    if (isNaN(number)){
+        return"Please enter a valid number.";
+    }
+    if (number<0|| number>100){
+        return "Please guess a number between 1 and 100.";
+    }
+    return true;
+}
+async function askForGuess() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'guess',
+        message: "Please guess a number between 1 and 100:",
+        validate: validNumber,
+    }])
+    .then ((answer: any)=>{
+        const guessedNumber = parseFloat(answer.guess)
+        if(guessedNumber === randomNumber){
+        console.log(chalk.bgBlue.bgMagentaBright(`Congrats! You guessed the number ${randomNumber} correctly.`));
+        process.exit(0)
+    }else if (guessedNumber < randomNumber){
+        remainingChances--;
+        console.log(chalk.bgBlue.bgRedBright(`Guess again, your guessed number is very low.
+        Now your remaining chances are:${remainingChances}`));   
+
+    if(remainingChances == 0){
+        console.log(chalk.bgMagentaBright.bgYellowBright(`Sorry! You have no more chance.
+        Correct number is ${randomNumber}`));
+    } else{
+        askForGuess();
+    }}
+    else if (guessedNumber > randomNumber){
+        remainingChances--;
+        console.log(chalk.bgBlue.bgRedBright(`Guess again, your guessed number is very high.
+        Now your remaining chances are:${remainingChances}`)); 
+
+    if(remainingChances == 0){
+        console.log(chalk.bgMagentaBright.bgYellowBright(`Sorry! You have no more chance. 
+     Correct number is ${randomNumber}`));
+    }else{
+        askForGuess();
+
+    }} 
+    })
+}
+askForGuess();
